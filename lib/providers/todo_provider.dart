de41ec5b4e02f35a9a7954b5ad28b69d0ebee505
Misbah <pendosa427@gmail.com>
2025-05-28 07:22:35 +0700

    Tambah TodoProvider

---
+++ lib/providers/todo_provider.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../models/todo.dart';
+
+class TodoProvider extends ChangeNotifier {
+  final List<Todo> _todos = [];
+
+  List<Todo> get todos => _todos;
+
+  void addTodo(String title, DateTime deadline) {
+    _todos.add(
+      Todo(title: title, createdAt: DateTime.now(), deadline: deadline),
+    );
+    notifyListeners();
+  }
+
+  void editTodo(
+    int index,
+    String newTitle,
+    DateTime newDeadline,
+    String detail,
+  ) {
+    final todo = _todos[index];
+    todo.title = newTitle;
+    todo.deadline = newDeadline;
+    todo.detail = detail;
+    notifyListeners();
+  }
+
+  void deleteTodo(int index) {
+    _todos.removeAt(index);
+    notifyListeners();
+  }
+
+  void restoreTodo(int index, Todo todo) {
+    _todos.insert(index, todo);
+    notifyListeners();
+  }
+
+  void toggleCompleted(int index) {
+    _todos[index].completed = !_todos[index].completed;
+    notifyListeners();
+  }
+}
