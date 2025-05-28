7f9cf046ab50c6baa6bd41c83db2af14403fe05b
Misbah <pendosa427@gmail.com>
2025-05-28 08:26:26 +0700

    Tambah Dialog Todo & ListView

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,66 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<String> todos = [];
+
+  void _addTodo(String newTodo) {
+    setState(() {
+      todos.add(newTodo);
+    });
+  }
+
+  void _showAddDialog() {
+    final TextEditingController controller = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            title: const Text("Tambah Todo"),
+            content: TextField(
+              controller: controller,
+              decoration: const InputDecoration(hintText: "Tulis todo..."),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Batal"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (controller.text.trim().isNotEmpty) {
+                    _addTodo(controller.text.trim());
+                  }
+                  Navigator.pop(context);
+                },
+                child: const Text("Tambah"),
+              ),
+            ],
+          ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: const Center(child: Text('No todos yet')),
+      appBar: AppBar(title: const Text('Daftar Todo')),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) => ListTile(title: Text(todos[index])),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showAddDialog,
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
